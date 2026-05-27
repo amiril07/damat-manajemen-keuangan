d8ea4bc34196cae608fb44fbfb315fa04a6d02f5
mamirilma <mamirilmukmininazzaky@gmail.com>
2026-05-27 16:10:41 +0700

    menambah css

--- pages/damgpt.php
+++ pages/damgpt.php
@@ -59,7 +59,7 @@ if (
     }
     $messages[] = ['role' => 'user', 'content' => $userMessage];
 
-   
+    $groqApiKey = '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';
     $model      = 'llama-3.1-8b-instant';
     $apiUrl     = 'https://api.groq.com/openai/v1/chat/completions';
 
